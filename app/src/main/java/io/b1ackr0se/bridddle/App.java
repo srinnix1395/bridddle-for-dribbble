7bd0a6217fc09e0c0af2b83e607910fb80498773
Tuha_Ominext <tuha@ominext.com>
2017-06-12 17:45:58 +0700

    12/6/2017

--- app/src/main/java/io/b1ackr0se/bridddle/App.java
+++ app/src/main/java/io/b1ackr0se/bridddle/App.java
@@ -4,12 +4,9 @@ package io.b1ackr0se.bridddle;
 import android.app.Application;
 import android.content.Context;
 
-import com.crashlytics.android.Crashlytics;
-
 import io.b1ackr0se.bridddle.injection.components.AppComponent;
 import io.b1ackr0se.bridddle.injection.components.DaggerAppComponent;
 import io.b1ackr0se.bridddle.injection.modules.AppModule;
-import io.fabric.sdk.android.Fabric;
 
 public class App extends Application {
     AppComponent appComponent;
@@ -22,7 +19,6 @@ public class App extends Application {
                 .build(); 
         appComponent.inject(this);
 
-        Fabric.with(this, new Crashlytics());
     }
 
     public static App getApplication(Context context) {
